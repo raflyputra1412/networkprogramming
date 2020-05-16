20c58763e15112d5aa483c7bec590404b032dee2
Raflyputra <raflyputra221b@gmail.com>
2020-05-17 03:52:26 +0700

    fixin some code

--- mail/google-mail-pop3.py
+++ mail/google-mail-pop3.py
@@ -1,28 +1,53 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 5
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
+#Muhammad Rafly Hersa Putra
+#170010009
 
-import argparse
 import getpass
 import poplib
+import imaplib
+
+IMAP_SERVER = 'imap.gmail.com'
+IMAP_PORT = 993
+
+POP_SERVER = 'pop.gmail.com'
+POP_PORT = 995
 
 
+def imap_mail(username):
+        mailbox = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT)
+        password = getpass.getpass(prompt='Enter your email password: ')
+        mailbox.login(username[0], password)
+        mailbox.select('Inbox')
+        typ, data = mailbox.search(None, 'ALL')
+        for num in data[0].split():
+            typ, data = mailbox.fetch(num, '(RFC822)')
+            print ('Message %s\n%s\n' % (num, data[0][1]))
+            break
+        mailbox.close()
+        mailbox.logout()
 
-GOOGLE_POP3_SERVER = 'pop.googlemail.com'
+def pop_mail(username):
+        mailbox = poplib.POP3_SSL(POP_SERVER, POP_PORT)
+        mailbox.user(username[0])
+        password = getpass.getpass(prompt='Enter your email password: ')
+        mailbox.pass_(password)
+        num_messages = len(mailbox.list()[1])
+        print ('Total emails: {}'.format(num_messages))
+        mailbox.quit()
 
-def download_email(username): 
-    mailbox = poplib.POP3_SSL(GOOGLE_POP3_SERVER, '995') 
-    mailbox.user(username)
-    password = getpass.getpass(prompt='Enter your Google password: ') 
-    mailbox.pass_(password) 
-    num_messages = len(mailbox.list()[1])
-    print ('Total emails: {}'.format(num_messages))
-    mailbox.quit()
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Email Download Example')
-    parser.add_argument('--username', action="store", dest="username", default=getpass.getuser())
-    given_args = parser.parse_args() 
-    username = given_args.username
-    download_email(username)
+def mail(username, protocol, host, port):
+    data = [username, protocol, host, port]
+    if (protocol == 1):
+        imap_mail(data)
+
+    elif (protocol == 2):
+        pop_mail(data)
+
+username = input("input email: ")
+try:
+    print("type 1 for imap or type 2 for pop3.")
+    protocol = int(input("protocol: "))
+except:
+    print("hmm there is something wrong here...")
+
+mail(username, protocol, host, port)
